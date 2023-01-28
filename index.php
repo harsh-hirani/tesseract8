ada3a728c2bc1e6be9491540e107826f98f5a433
Harsh Hirani <hiraniharsh7@gmail.com>
2023-01-28 14:05:00 +0530

    finally sidebar done

--- index.php
+++ index.php
@@ -30,28 +30,28 @@
                 <!-- 000000000 -->
                 <!-- links -->
                 <div class="link con" id="linkCon">
-                    <div class="link item div active" onclick="scrollerFunction('mainBG')">
+                    <div class="link item div active" onclick="scrollerFunction('mainBG',this)">
                         <div class="conted">
                             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0,0,256,256" width="32px" height="32px" fill-rule="nonzero"><g fill="#5a85fc" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(8,8)"><path d="M16,2.59375l-0.71875,0.6875l-13,13l1.4375,1.4375l1.28125,-1.28125v11.5625h9v-10h4v10h9v-11.5625l1.28125,1.28125l1.4375,-1.4375l-13,-13zM16,5.4375l9,9v11.5625h-5v-10h-8v10h-5v-11.5625z"></path></g></g></svg>
                             <span>Home</span>
                         </div>
                     </div>
-                    <div class="link item div" onclick="scrollerFunction('edm')">
+                    <div class="link item div" onclick="scrollerFunction('edm',this)">
                         <div class="conted">
                             <img src="edm.png"/>
                             <span>EDM</span>
                         </div>
                     </div>
-                    <div class="link item div" onclick="scrollerFunction('gallery')">
+                    <div class="link item div" onclick="scrollerFunction('gallery',this)">
                         <div class="conted"><img src="gal.png"/><span>Gallery</span></div>
                     </div>
-                    <div class="link item div" onclick="scrollerFunction('event')">
+                    <div class="link item div" onclick="scrollerFunction('event',this)">
                         <div class="conted"><img src="event.png"/><span>Event</span></div>
                     </div>
-                    <div class="link item div" onclick="scrollerFunction('shed')">
+                    <div class="link item div" onclick="scrollerFunction('shed',this)">
                         <div class="conted"><img src="shed.png"/><span>Schedule</span></div>
                     </div>
-                    <div class="link item div" onclick="scrollerFunction('about')">
+                    <div class="link item div" onclick="scrollerFunction('about',this)">
                         <div class="conted"><img src="about.png"/><span>About</span></div>
                     </div>
                 </div>
@@ -59,7 +59,7 @@
         </div>
     </section>
     <main id="content" class="my-scroller">
-        <section id="mainBG">
+        <section id="mainBG" data-key="0">
             <img src="mainBG_hero.png"/>
             <div class="firstTextBox">
                 <div style="heighgt:100%"><img src="logo.png" id="justforview" style="height:150px;object-fit:cover; margin-right:20px;"></div>
@@ -72,14 +72,19 @@
                 </div>
             </div>
         </section>
-        <section id="second">
+        <section id="second" data-key="0">
             <div class="container">
                 <h1 class="title gold">Great start of 2023</h1>
                 <p class="text">Tesseract, the annual Science and Technical Fest of PDEU, is one of Gujarat's biggest technical college fests. Tesseract provides a platform for tech nerds from colleges such as Pandit Deendayal Energy University and many others to interact, share and celebrate aspirations, creativity, and innovations. It attracts over 6,000 people each year and gives all the students who attend a platform to network and learn about cutting-edge developments in the areas of science and technology.</p>
             </div>
         </section>
+
+        <!-- edm -->
+        <section id="edm" data-key="1">
+            <div class="mix title">EDM</div>
+        </section>
         <!-- gallery -->
-        <section id="gallery">
+        <section id="gallery" data-key="2">
             <div class="green title"> Gallery Room</div>
             <p class="text capitalize">click botton to Load gallery</p>
             <div id="galleryBinder">
@@ -89,37 +94,67 @@
                 </div>
             </div>
         </section>
-        <section id="event">
+        <section id="event" data-key="3">
         <div class="gold title"> Events </div>
         </section>
-        <section id="shed">
+        <section id="shed" data-key="4">
         <div class="green title"> Scheduls </div>
         </section>
-        <section id="about">
+        <section id="about" data-key="5">
         <div class="gold title"> About </div>
         </section>
     </main>
     </div>
-    <script src="jquery-3.6.0.min.js"></script>
+    <script src="jquery-3.6.0.js"></script>
  <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" crossorigin="anonymous"></script> -->
     <script>
         var firstTimeLoad = true;
         //navigation functions
-        function scrollerFunction(e){
-            document.getElementById(e).scrollIntoView();
+        function scrollerFunction(e,elm){
+            document.getElementById(e).scrollIntoView({ behavior: 'smooth' });
             if(window.innerWidth < 768){
                 myswitch();
             }
-        }
-        $('.item').click(function(e) {
-
             $('.item').removeClass('active');
+            elm.classList.add('active');
+        }
+        // // event handler for scroll behaviour handler
+        let mainNavLinks = document.querySelectorAll(".link.item.div");
+        let mainSections = document.querySelectorAll("main section");
+        function changer(i){
+            $('.item').removeClass('active');
+            mainNavLinks[i].classList.add("active");
+        }
+       // document.getElementById("content").addEventListener("scroll", event => {
+        //    let fromTop = window.scrollY;
+                /*if($("#second").is(":visible")) {changer(0);
+                }else*/
+                // if($("#about").is(":visible")) {changer(5);
+                 //}else if($("#shed").is(":visible")) {changer(4);
+                 //}else if($("#event").is(":visible")) {changer(3);
+                 //}else if($("#gallery").is(":visible")) {changer(2);
+                //}else if($("#edm").is(":visible")) {changer(1);
+                //}else if($("#mainBG").is(":visible")) {changer(0);
+        //        }
+    //});
+        $.fn.isInViewport = function() {
+            var elementTop = $(this).offset().top;
+            var elementBottom = elementTop + $(this).outerHeight();
 
-            var $this = $(this);
-            if (!$this.hasClass('active')) {
-                $this.addClass('active');
+            var viewportTop = $(window).scrollTop();
+            var viewportBottom = viewportTop + $(window).height();
+
+            return elementBottom > viewportTop && elementTop < viewportBottom;
+        };
+        $("#content").scroll(function() {
+            if ($('#about').isInViewport()) {changer(5);
+            }else if ($('#shed').isInViewport()) {changer(4);
+            }else if ($('#event').isInViewport()) {changer(3);
+            }else if ($('#gallery').isInViewport()) {changer(2);
+            }else if ($('#edm').isInViewport()) {changer(1);
+            }else if ($('#second').isInViewport()) {changer(0);
+            }else if ($('#mainBG').isInViewport()) {changer(0);
             }
-            //e.preventDefault();
         });
         let switchcase = false;
         function myswitch(){
